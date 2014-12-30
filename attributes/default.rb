06fb418c786a095dd273006e708e88921cda50f9
Eric Gay <eric.gay@gmail.com>
2014-12-30 16:10:27 -0500

    add default for config dir

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,6 +2,9 @@ include_attribute 'deploy'
 
 default[:sidekiq] = {}
 
+default[:sidekiq][:monit] = {}
+default[:sidekiq][:monit][:conf_dir] = "/etc/monit.d/"
+
 node[:deploy].each do |application, deploy|
   default[:sidekiq][application.intern] = {}
   default[:sidekiq][application.intern][:restart_command] = "sudo monit restart -g sidekiq_#{application}_group"
